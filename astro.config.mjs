5f18fec2ca11ac131c87334eca086ccb6c673de0
DominicDev <s0l0estudi4r@gmail.com>
2025-04-01 15:40:29 +0200

    v0.1.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  base: ".",
+  base: '.',
   integrations: [icon()]
 });
